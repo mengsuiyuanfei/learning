c5b78c7a0b43762c5c441d03cdbaecc06b221a2a
lihongfei <741064960@qq.com>
2022-03-30 22:15:22 +0800

    测试

---
+++ IdeaProjects/LHFTest/src/main/java/test.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang.StringUtils;
+
+public class test {
+    public void test1(){
+        String a="";
+        String b=" ";
+        if(StringUtils.not)
+    }
+}
--- IdeaProjects/javaBase/src/Demo03_Math.java
+++ IdeaProjects/javaBase/src/Demo03_Math.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * abs:绝对值
  * ceil:向上取整
@@ -6,4 +9,55 @@
  */
 
 public class Demo03_Math {
+    public static void main(String[] args) {
+        String [] numStr ={"3","5","6","8","11"};
+        int target=16;
+        int [] result=sumTwo(numStr,target);
+        for(int a:result){
+            System.out.println(a);
+        }
+    }
+
+
+
+
+    public static int[] sumTwo(String[] nums,int target){
+        Map<String,Integer> map=new HashMap<>();
+        for(int i=0;i < nums.length;i++){
+            int result=target-Integer.valueOf(nums[i]);
+            if(map.containsKey(String.valueOf(result))){
+                return new int[]{map.get(String.valueOf(result)),i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("不存在！");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static int[] twoSum(String[] nums,int target){
+        Map<String,Integer> map=new HashMap<>();
+        for(int i=0;i< nums.length;i++){
+            int result=target-Integer.valueOf(nums[i]);
+            if(map.containsKey(String.valueOf(result))){
+                return new int[]{map.get(String.valueOf(result)),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("无结果！");
+    }
 }
